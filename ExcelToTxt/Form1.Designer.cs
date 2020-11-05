2a3d054445049d724ba907e8ccd0e20e60e8b1e4
king1314okok <759046694@qq.com>
2020-11-05 21:39:32 +0800

    init

---
+++ ExcelToTxt/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace ExcelToTxt
+{
+  partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources =
+                new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.ui_InputPathText = new System.Windows.Forms.TextBox();
+            this.ui_ExcelPathBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ui_OutputPathText = new System.Windows.Forms.TextBox();
+            this.ui_TxtPathBtn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.ui_FileNameText = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ui_StartBtn = new System.Windows.Forms.Button();
+            this.ui_KeepNameCheckBox = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ui_ResultText = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(28, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Excel文件路径:";
+            // 
+            // ui_InputPathText
+            // 
+            this.ui_InputPathText.Location = new System.Drawing.Point(143, 21);
+            this.ui_InputPathText.Name = "ui_InputPathText";
+            this.ui_InputPathText.Size = new System.Drawing.Size(473, 23);
+            this.ui_InputPathText.TabIndex = 1;
+            // 
+            // ui_ExcelPathBtn
+            // 
+            this.ui_ExcelPathBtn.Location = new System.Drawing.Point(623, 16);
+            this.ui_ExcelPathBtn.Name = "ui_ExcelPathBtn";
+            this.ui_ExcelPathBtn.Size = new System.Drawing.Size(89, 33);
+            this.ui_ExcelPathBtn.TabIndex = 2;
+            this.ui_ExcelPathBtn.Text = "打开";
+            this.ui_ExcelPathBtn.UseVisualStyleBackColor = true;
+            this.ui_ExcelPathBtn.Click += new System.EventHandler(this.ui_ExcelPathBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(28, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Txt保存路径:";
+            // 
+            // ui_OutputPathText
+            // 
+            this.ui_OutputPathText.Location = new System.Drawing.Point(143, 64);
+            this.ui_OutputPathText.Name = "ui_OutputPathText";
+            this.ui_OutputPathText.Size = new System.Drawing.Size(473, 23);
+            this.ui_OutputPathText.TabIndex = 1;
+            // 
+            // ui_TxtPathBtn
+            // 
+            this.ui_TxtPathBtn.Location = new System.Drawing.Point(623, 59);
+            this.ui_TxtPathBtn.Name = "ui_TxtPathBtn";
+            this.ui_TxtPathBtn.Size = new System.Drawing.Size(89, 33);
+            this.ui_TxtPathBtn.TabIndex = 2;
+            this.ui_TxtPathBtn.Text = "打开";
+            this.ui_TxtPathBtn.UseVisualStyleBackColor = true;
+            this.ui_TxtPathBtn.Click += new System.EventHandler(this.ui_TxtPathBtn_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ui_TxtPathBtn);
+            this.groupBox1.Controls.Add(this.ui_ExcelPathBtn);
+            this.groupBox1.Controls.Add(this.ui_OutputPathText);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.ui_InputPathText);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 11);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(717, 98);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "文件路径";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.ui_FileNameText);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.ui_StartBtn);
+            this.groupBox2.Controls.Add(this.ui_KeepNameCheckBox);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(12, 135);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(717, 186);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "转换选项";
+            // 
+            // ui_FileNameText
+            // 
+            this.ui_FileNameText.Location = new System.Drawing.Point(115, 54);
+            this.ui_FileNameText.Name = "ui_FileNameText";
+            this.ui_FileNameText.Size = new System.Drawing.Size(235, 23);
+            this.ui_FileNameText.TabIndex = 9;
+            this.ui_FileNameText.Text = "TranslatedFileName";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(30, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "2.输出文件名";
+            // 
+            // ui_StartBtn
+            // 
+            this.ui_StartBtn.Font = new System.Drawing.Font("Microsoft YaHei UI", 20F);
+            this.ui_StartBtn.Location = new System.Drawing.Point(409, 23);
+            this.ui_StartBtn.Name = "ui_StartBtn";
+            this.ui_StartBtn.Size = new System.Drawing.Size(303, 157);
+            this.ui_StartBtn.TabIndex = 7;
+            this.ui_StartBtn.Text = "转换";
+            this.ui_StartBtn.UseVisualStyleBackColor = true;
+            this.ui_StartBtn.Click += new System.EventHandler(this.ui_StartBtn_Click);
+            // 
+            // ui_KeepNameCheckBox
+            // 
+            this.ui_KeepNameCheckBox.Checked = true;
+            this.ui_KeepNameCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ui_KeepNameCheckBox.Location = new System.Drawing.Point(115, 27);
+            this.ui_KeepNameCheckBox.Name = "ui_KeepNameCheckBox";
+            this.ui_KeepNameCheckBox.Size = new System.Drawing.Size(59, 20);
+            this.ui_KeepNameCheckBox.TabIndex = 4;
+            this.ui_KeepNameCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(30, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "1.保留行名";
+            // 
+            // ui_ResultText
+            // 
+            this.ui_ResultText.Location = new System.Drawing.Point(421, 112);
+            this.ui_ResultText.Name = "ui_ResultText";
+            this.ui_ResultText.Size = new System.Drawing.Size(230, 20);
+            this.ui_ResultText.TabIndex = 8;
+            this.ui_ResultText.Text = "Excel文件不存在";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(742, 333);
+            this.Controls.Add(this.ui_ResultText);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon) (resources.GetObject("$this.Icon")));
+            this.Location = new System.Drawing.Point(15, 15);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "编程王精品工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ui_InputPathText;
+        private System.Windows.Forms.TextBox ui_OutputPathText;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox ui_KeepNameCheckBox;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label ui_ResultText;
+        private System.Windows.Forms.Button ui_StartBtn;
+        private System.Windows.Forms.Button ui_TxtPathBtn;
+        private System.Windows.Forms.Button ui_ExcelPathBtn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox ui_FileNameText;
+    }
+}
---
+++ ExcelToTxt/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Spire.Xls;
+using System.Windows.Forms;
+
+namespace ExcelToTxt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ui_ResultText.Text = "";
+        }
+        
+        void LoadExcel(string inputPath,string outputPath,bool keepName = true)
+        {
+            Workbook workbook = new Workbook();
+            workbook.LoadFromFile(inputPath);
+            Worksheet sheet = workbook.Worksheets[0];
+            FileStream fs = new FileStream(outputPath, FileMode.Create);
+            Encoding utf8 = Encoding.UTF8;
+            int index = keepName ? 0 : 1;
+            for (;index < sheet.Rows.Length; index++)
+            {
+                var row = sheet.Rows[index];
+                string content = "";
+                for (int i = 0; i < row.Count; i++)
+                {
+                    if (i > 0)
+                        content += "|";
+                    string item = row.Cells[i].Value.Replace((char) 0x0A, (char) 0x1E).Replace('|', (char) 0x1F);
+                    content += item;
+                }
+
+                content += (char) 0x0A;
+                byte[] bytes = utf8.GetBytes(content);
+                fs.Write(bytes, 0, bytes.Length);
+            }
+            fs.Close();
+        }
+
+        void LoadCSV(string inputPath,string outputPath,bool keepName = true)
+        {
+            using (StreamReader  sr = new StreamReader (inputPath,Encoding.Default))
+            {
+                FileStream fs = new FileStream(outputPath, FileMode.Create);
+                Encoding utf8 = Encoding.UTF8;
+                string curLine;
+                if (!keepName)
+                    sr.ReadLine();
+                while ((curLine = sr.ReadLine()) != null)
+                {
+                    string[] items = curLine.Split(',');
+                    string content = "";
+                    for (int i = 0; i < items.Length; i++)
+                    {
+                        if (i > 0)
+                            content += "|";
+                        string item = items[i].Replace((char) 0x0A, (char) 0x1E).Replace('|', (char) 0x1F);
+                        content += item;
+                    }
+                    content += (char) 0x0A;
+                    byte[] bytes = utf8.GetBytes(content);
+                    fs.Write(bytes, 0, bytes.Length);
+                }
+                fs.Close();
+            }
+        }
+
+        private void ui_ExcelPathBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog(this) == DialogResult.OK)
+            {
+                ui_InputPathText.Text = dialog.FileName;
+            }
+        }
+
+        private void ui_TxtPathBtn_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog(this) == DialogResult.OK)
+            {
+                ui_OutputPathText.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void ui_StartBtn_Click(object sender, EventArgs e)
+        {
+            string inputPath = ui_InputPathText.Text;
+            string outputPath = ui_OutputPathText.Text;
+            if (!File.Exists(inputPath) || !Directory.Exists(outputPath))
+            {
+                ui_ResultText.Text = "目标文件或路径不存在";
+                return;
+            }
+
+            string fileName = ui_FileNameText.Text;
+            if (fileName.Trim() == "")
+            {
+                ui_ResultText.Text = "输出文件名不能为空";
+                return;
+            }
+
+            outputPath = outputPath + "//" + fileName + ".txt";
+            FileInfo fileInfo = new FileInfo(inputPath);
+            try
+            {
+                bool keepName = ui_KeepNameCheckBox.Checked;
+                if (fileInfo.Extension == ".csv")
+                    LoadCSV(inputPath, outputPath,keepName);
+                else
+                    LoadExcel(inputPath, outputPath,keepName);
+                
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            ui_ResultText.Text = "转换成功";
+        }
+    }
+}
